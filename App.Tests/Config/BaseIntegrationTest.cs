1b0b9eb22c71d8741b7fe8858c2a3dc13f0d33bc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-28 17:17:28 -0300

    Improvements

--- App.Tests/Config/BaseIntegrationTest.cs
+++ App.Tests/Config/BaseIntegrationTest.cs
@@ -1,4 +1,5 @@
 using App.Config.Database;
+using App.Config.Tx;
 using App.Modules.User.Model;
 using App.Modules.User.Repositories;
 using Bogus;
@@ -7,46 +8,71 @@ using Microsoft.Extensions.DependencyInjection;
 namespace App.Tests.Config;
 
 [CollectionDefinition("Integration Tests")]
-public class IntegrationCollection : ICollectionFixture<DatabaseFixture> { }
+public class IntegrationCollection : ICollectionFixture<DatabaseFixture>
+{
+}
 
 [Collection("Integration Tests")]
-public abstract class BaseIntegrationTest
+public abstract class BaseIntegrationTest : IAsyncDisposable
 {
     protected readonly HttpClient Client;
     protected readonly DatabaseFixture Factory;
-    protected readonly Faker _faker = new("pt_BR");
-    protected readonly UserRepository _userRepository;
-    
+    protected readonly Faker Faker = new("pt_BR");
+
+    protected readonly IServiceScope Scope;
+    protected readonly UserRepository UserRepository;
+
     protected BaseIntegrationTest(DatabaseFixture factory)
     {
         Factory = factory;
-        Client = factory.CreateClient();
-        
-        using var scope = factory.Services.CreateScope();
-        IDatabase database = scope.ServiceProvider.GetRequiredService<IDatabase>();
 
-        _userRepository = new UserRepository(database);
+        Client = factory.CreateClient();
+
+        Scope = factory.Services.CreateScope();
+
+        IDatabase database =
+            Scope.ServiceProvider.GetRequiredService<IDatabase>();
+        
+        var session = new TestDbSession(database);
+
+        
+        UserRepository = new UserRepository(session);
     }
 
-    public async Task<UserEntity> CreateUser() {
-        var id = _faker.Random.Long(10000000, 9999999999);
+
+    public async Task<UserEntity> CreateUser()
+    {
+        var id = Faker.Random.Long(10000000, 9999999999);
+
         var user = new UserEntity
         {
-            Id = id, 
+            Id = id,
             UserName = "Pochita " + id,
-            Email = _faker.Internet.Email(),
-            FullName = _faker.Name.FullName(),
-            PasswordHash = _faker.Internet.Password(),
-            NormalizedUserName = _faker.Internet.UserName(),
-            NormalizedEmail = _faker.Internet.Email(),
+            Email = Faker.Internet.Email(),
+            FullName = Faker.Name.FullName(),
+            PasswordHash = Faker.Internet.Password(),
+            NormalizedUserName = Faker.Internet.UserName(),
+            NormalizedEmail = Faker.Internet.Email(),
             Active = true,
-            CreatedAt = DateTimeOffset.UtcNow,
-            UpdatedAt = DateTimeOffset.UtcNow
+            CreatedAt = DateTime.UtcNow,
+            UpdatedAt = DateTime.UtcNow
         };
 
-        await _userRepository.CreateAsync(user);
-        
+        await UserRepository.CreateAsync(user);
+
         return user;
     }
-    
+
+
+    public async ValueTask DisposeAsync()
+    {
+        if (Scope is IAsyncDisposable asyncScope)
+        {
+            await asyncScope.DisposeAsync();
+        }
+        else
+        {
+            Scope.Dispose();
+        }
+    }
 }
