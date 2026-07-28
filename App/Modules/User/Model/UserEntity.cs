5f7a7024ebd9652a41dcad139bdfeb3d83c2d16a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-28 17:19:58 -0300

    Added : BaseEntity

--- App/Modules/User/Model/UserEntity.cs
+++ App/Modules/User/Model/UserEntity.cs
@@ -1,19 +1,24 @@
-using Microsoft.AspNetCore.Identity;
-
+using App.Utils.Base.Entity;
 namespace App.Modules.User.Model;
 
-public class UserEntity : IdentityUser<long>
-{       
-    public readonly static string _tableName = "users";
-    
+public sealed class UserEntity: BaseEntity
+{
+    public const string _tableName = "users";
+
+    public string UserName { get; set; } = null!;
+
+    public string NormalizedUserName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string NormalizedEmail { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
     public string FullName { get; set; } = null!;
 
     public bool Active { get; set; } = true;
 
-    public DateTimeOffset CreatedAt { get; set; }
-
-    public DateTimeOffset UpdatedAt { get; set; }
 
     public DateTimeOffset? LastLoginAt { get; set; }
-    
-}
+}    
