38cbc808f978579eae00aeb3ac76835343f9b186
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-29 10:54:13 -0300

    Added repository to user roles

---
+++ App/Modules/UserRole/Repositories/UserRoleRepository.cs
@@ -0,0 +1,60 @@
+using System.Text;
+using App.Config.Database.Session;
+using App.Modules.Role.Model;
+using App.Modules.User.Model;
+using App.Modules.UserRole.Model;
+using App.Utils.Base.Repository;
+using Dapper;
+
+namespace App.Modules.UserRole.Repositories;
+
+public class UserRoleRepository(IDatabaseSession database)
+    : BaseRepositoryImpl<UserRoleEntity, long>(database, UserRoleEntity._tableName),
+        IUserRoleRepository
+{
+    public async Task<UserRoleEntity?> GetByUserIdAndRoleId(
+        long userId,
+        long roleId)
+    {
+        const string sql = $"""
+                            SELECT *
+                            FROM {UserRoleEntity._tableName}
+                            WHERE user_id = @UserId
+                              AND role_id = @RoleId;
+                            """;
+
+        return await Database.Connection.QuerySingleOrDefaultAsync<UserRoleEntity>(
+            sql,
+            new
+            {
+                UserId = userId,
+                RoleId = roleId
+            },
+            Database.Transaction);
+    }
+    
+    public async Task<bool> ExistsByUserIdAndRoleId(
+        long userId,
+        long roleId)
+    {
+        const string sql = $"""
+                           SELECT EXISTS (
+                               SELECT 1
+                               FROM {UserRoleEntity._tableName}
+                               WHERE user_id = @UserId
+                                 AND role_id = @RoleId
+                                 AND active = TRUE
+                           );
+                           """;
+
+        return await Database.Connection.ExecuteScalarAsync<bool>(
+            sql,
+            new
+            {
+                UserId = userId,
+                RoleId = roleId
+            },
+            Database.Transaction);
+    }
+    
+}
