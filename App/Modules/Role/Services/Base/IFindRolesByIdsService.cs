0d690081a8fdf2339c2895e98181399ff24b1005
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-31 15:36:50 -0300

    Impled logic, test and interface to find roles by ids

---
+++ App/Modules/Role/Services/Base/IFindRolesByIdsService.cs
@@ -0,0 +1,9 @@
+using App.Modules.Role.Model;
+using App.Utils.Result;
+
+namespace App.Modules.Role.Services.Base;
+
+public interface IFindRolesByIdsService
+{
+    Task<Result<List<RoleEntity>>> FindByIdsAsync(List<long> ids);
+}
